ead3639d1dad286aaf766a0ac3553af3ef3111bf
avigupta250 <avinashkumarnav52@gmail.com>
2024-12-17 10:47:38 +0530

    skill

--- src/components/SkillSection.js
+++ src/components/SkillSection.js
@@ -16,7 +16,7 @@ const SkillSection = () => {
                     <div className='rounded-md bg-[#04060c] text-xs p-2 border hover:scale-105 duration-200 border-blue-700'>C++</div>
                     {/* <div className='rounded-md bg-[#04060c] text-xs p-2 border hover:scale-105 duration-200 border-[#5ed76491]'></div> */}
                     <div className='rounded-md bg-[#04060c] text-xs p-2 border hover:scale-105 duration-200 border-[#b73d3d]'>Javascript</div>
-                    {/* <div className='rounded-md bg-[#04060c] text-xs p-2 border hover:scale-105 duration-200 border-[#e58d4d]'>Python</div> */}
+                    <div className='rounded-md bg-[#04060c] text-xs p-2 border hover:scale-105 duration-200 border-[#e58d4d]'>TypeScript</div>
                 </div>
 
             </div>
